eb9f1a90398dbf2c72cf473fe0e744a81de9e121
Andrew Hing <131317473+ahing1@users.noreply.github.com>
2026-05-25 22:21:17 -0400

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        res = []
+
+        def isPalindrone(s):
+            return s == s[::-1]
+
+        def backtrack(start, path):
+            if start == len(s):
+                res.append(path.copy())
+            
+            for i in range(start, len(s)):
+                if isPalindrone(s[start:i+1]):
+                    path.append(s[start:i+1])
+                    backtrack(i + 1, path)
+                    path.pop()
+        
+        backtrack(0, [])
+        return res
+            
+            
